842200dc1a5daf54ba4e368108f2e183b285ef63
radoslawstasiak <radoslaw.stasiak@news.co.uk>
2021-12-16 21:56:43 +0100

    add basic dashboard options using compose

--- app/src/main/java/io/radev/roman/DashboardActivity.kt
+++ app/src/main/java/io/radev/roman/DashboardActivity.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.Help
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.radev.roman.ui.theme.RomanappTheme
 
 class DashboardActivity : ComponentActivity() {
@@ -37,11 +48,12 @@ class DashboardActivity : ComponentActivity() {
 fun RomanApp() {
     Surface(color = MaterialTheme.colors.background) {
         Column(
-            verticalArrangement = Arrangement.Center,
+//            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
+                .padding(vertical = 8.dp, horizontal = 8.dp)
         ) {
             DashboardMenuItems()
         }
@@ -56,9 +68,11 @@ fun DashboardMenuItems() {
         items(itemList) { item ->
             Card(
                 backgroundColor = MaterialTheme.colors.primary,
+                shape = MaterialTheme.shapes.small.copy(CornerSize(percent = 15)),
                 modifier = Modifier
-                    .width(200.dp)
-                    .height(50.dp)
+                    .fillMaxWidth()
+                    .defaultMinSize(minHeight = 100.dp)
+//                    .height(100.dp)
                     .padding(vertical = 4.dp)
                     .clickable {
                         when (item) {
@@ -72,26 +86,70 @@ fun DashboardMenuItems() {
             }
         }
     }
+
 }
 
 @Composable
 fun CardContent(title: String) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Icon(
-            imageVector = when (title) {
-                "Travel" -> Icons.Filled.TravelExplore
-                "Day" -> Icons.Filled.Restaurant
-                "Set reminder" -> Icons.Filled.AddAlert
-                else -> Icons.Filled.TripOrigin
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(top = 16.dp)
+
+        ) {
+            Icon(
+                imageVector = when (title) {
+                    "Travel" -> Icons.Filled.TravelExplore
+                    "Day" -> Icons.Filled.Restaurant
+                    "Set reminder" -> Icons.Filled.AddAlert
+                    else -> Icons.Filled.TripOrigin
+                },
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 8.dp)
+            )
+            Text(
+                modifier = Modifier
+                    .weight(1f),
+                text = title,
+                style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.Bold)
+
+            )
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = Icons.Outlined.Help,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(end = 16.dp)
+                )
+            }
+        }
+
+        if (expanded) Text(
+            text = when (title) {
+                "Travel" -> "Plan a trip together"
+                "Day" -> "Plan a night out together"
+                "Set reminder" -> "Set a reminder to stay romantic"
+                else -> ""
             },
-            contentDescription = "",
-            modifier = Modifier.padding(horizontal = 8.dp)
+//            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.body2.copy(fontSize = 22.sp)
         )
-        Text(
-            text = title,
-//            modifier = Modifier.padding(8.dp)
-        )
+        //todo check how to add spring animation for the layout click
     }
+
 }
 
 
@@ -109,7 +167,7 @@ fun DashboardItemPreview() {
 
 @Preview(
     showBackground = true,
-    showSystemUi = true
+    showSystemUi = false
 )
 @Composable
 fun DefaultPreview() {
