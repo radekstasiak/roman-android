8aa9701a5ef093536b37597a9fb83682b749cf6b
radekstasiak <radek.stasiak@gmail.com>
2022-02-18 22:25:47 +0100

    add ktor

---
+++ app/src/main/java/io/radev/roman/data/KtorService.kt
@@ -0,0 +1,12 @@
+package io.radev.roman.data
+
+import io.ktor.client.*
+/*
+ * Created by Radoslaw on 18/02/2022.
+ * radev.io 2022.
+ * Peace and Love.
+ */
+
+val client = HttpClient()
+class KtorService {
+}
