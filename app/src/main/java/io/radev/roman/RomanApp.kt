207fc275a24a322efb96e9a6599afe9cff4972d8
radekstasiak <radek.stasiak@gmail.com>
2022-02-18 21:59:26 +0100

    implement Hilt with basic config

---
+++ app/src/main/java/io/radev/roman/RomanApp.kt
@@ -0,0 +1,13 @@
+package io.radev.roman
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/*
+ * Created by Radoslaw on 18/02/2022.
+ * radev.io 2022.
+ * Peace and Love.
+ */
+
+@HiltAndroidApp
+class RomanApplication : Application()
